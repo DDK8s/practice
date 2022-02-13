bec0c518eecc8d6f1b35a18f97bb8000fcbe8139
DKey <kaplan.daguzhiev@mail.ru>
2022-02-13 12:07:54 +0300

    Новое задание для коммита.

---
+++ 1/1.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var notUnique bool
+	newLetters := []string{""}
+	fmt.Printf("Enter a word: ")
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	word := sc.Text()
+
+	letters := strings.Split(word, "")
+	fmt.Println(letters)
+
+	newLetters = append(newLetters, letters...)
+
+	for i, v := range letters {
+		for d, s := range letters {
+
+			if d != 0 && s == letters[d-1] {
+				if i != d {
+
+					if v == s {
+						fmt.Println("Coincidence " + v + " и " + s)
+						notUnique = true
+						break
+					} else {
+						fmt.Println("Not a coincidence " + v + " и " + s)
+						if d != 0 && s == letters[d+1] {
+							break
+						}
+					}
+
+				}
+			}
+		}
+		if notUnique {
+			fmt.Println("There are no unique word")
+			return
+		} else {
+			fmt.Println("There are unique word")
+			return
+		}
+	}
+}
