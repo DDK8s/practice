a3d15b726b394b6a622dce5f69411fa26cc42a68
DKey <kaplan.daguzhiev@mail.ru>
2022-02-27 00:53:35 +0300

    /**/

--- 5/5.go
+++ 5/5.go
@@ -1,97 +1,128 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type elem struct {
-	name string
-	next *elem
+type element struct {
+	data int
+	next *element
 }
 
-type singleList struct {
+type sinList struct {
 	len  int
-	head *elem
+	head *element
 }
 
-func initList() *singleList {
-	return &singleList{}
+func main() {
+	numbers := [6]int{8, 9, 7, 1, 2, 3}
+	Act(numbers)
 }
 
-func (s *singleList) AddBack(name string) *elem{
-	elem := &elem{
-		name: name,
+func Act(numbers [6]int) [6]int {
+	x := 5
+	sinList := initiationList()
+	numbers = sinList.fillArray(numbers)
+
+	/* Ручное заполнение.
+	sinList.addInEnd(8)
+	sinList.addInEnd(9)
+	sinList.addInEnd(7)
+	sinList.addInEnd(1)
+	sinList.addInEnd(2)
+	sinList.addInEnd(3)
+	*/
+
+	current := sinList.head
+	var number [6]int
+
+	for i := 0; current != nil; i++ {
+		if current.data < x {
+			s := current.data
+			sinList.removeElement(i)
+			sinList.addInFront(s)
+		}
+		current = current.next
+	}
+
+	current = sinList.head
+	for i := 0; current != nil; i++ {
+		number[i] = current.data
+		current = current.next
+	}
+
+	// Вывести весь список.
+	/*err := sinList.showList()
+	if err != nil {
+		fmt.Println(err.Error())
+	}*/
+
+	return number
+}
+
+func initiationList() *sinList {
+	return &sinList{}
+}
+
+func (s *sinList) fillArray(numbers [6]int) [6]int {
+
+	for _, v := range numbers {
+		s.addInEnd(v)
+	}
+	return numbers
+}
+
+func (s *sinList) addInEnd(data int) {
+	element := &element{
+		data: data,
 	}
 	if s.head == nil {
-		s.head = elem
+		s.head = element
 	} else {
 		current := s.head
 		for current.next != nil {
 			current = current.next
 		}
-		current.next = elem
+		current.next = element
 	}
 	s.len++
-	return elem
 }
 
-func (s *singleList) showList() error {
+func (s *sinList) addInFront(data int) {
+	element := &element{
+		data: data,
+	}
 	if s.head == nil {
-		return fmt.Errorf("list is empty")
+		s.head = element
+	} else {
+		element.next = s.head
+		s.head = element
+	}
+	s.len++
+	return
+}
+
+// Удаляет элемент i из списка.
+func (s *sinList) removeElement(i int) {
+
+	current := s.head
+	j := 0
+	for j < i-1 {
+		j++
+		current = current.next
+	}
+	remove := current.next
+	current.next = remove.next
+	s.len--
+	return
+}
+
+func (s *sinList) showList() error {
+	if s.head == nil {
+		return fmt.Errorf("sinList is empty")
 	}
 	current := s.head
 	for current != nil {
-		fmt.Println(current.name)
+		fmt.Println(current.data)
 		current = current.next
 	}
 	return nil
-}
-
-func main() {
-	singleList := initList()
-	s := Act(singleList)
-
-	//Вывести весь список.
-	/*err := singleList.showList()
-	if err != nil {
-		fmt.Println(err.Error())
-	}*/
-	fmt.Println(s)
-}
-
-func Act(singleList *singleList) string{
-
-	m := make(map[*elem]interface{})
-	words := fill()
-	for i := range words {
-		singleList.AddBack(words[i])
-	}
-
-	current := singleList.head
-
-	for current.next != nil {
-		current = current.next
-	}
-	current.next = singleList.head.next.next
-
-
-	for current.next != nil {
-		for range m {
-			if _, ok := m[current]; ok {
-				loopPoint := current.next
-				fmt.Println("Loop found", current.next.name)
-				return loopPoint.name
-			}
-		}
-		m[current] = nil
-		current = current.next
-	}
-
-	// fmt.Println("Loop starting point is", fast.name)
-	return "Loop not found."
-}
-
-func fill() [6]string{
-	words := [6]string{"A", "B", "C", "D", "E"}
-	return words
 }
