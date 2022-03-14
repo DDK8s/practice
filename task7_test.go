8f601d45b4b64a5826fb82d08b46cd9c923468a2
DKey <kaplan.daguzhiev@mail.ru>
2022-03-14 20:29:15 +0300

    adding

---
+++ task7_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAct(t *testing.T) {
+	// Arrange.
+	numbers := [6]int{8, 9, 7, 1, 2, 3}
+	expected := 2
+
+	// Act.
+	result := Act(numbers)
+
+	// Assert.
+	if result != expected {
+		t.Errorf("Incorrect result. Expected %d, got %d", expected, result)
+		return
+	}
+
+}
